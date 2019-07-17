d86e10b5699eb4ab7a159deaddc42fb69a33368d
eduarde1 <e.castrobermudez@comunidad.unam.mx>
2019-07-16 23:46:21 -0500

    Actualicé el challenge 2 y avancé el 3 sin lograr resolverlo aún

--- challenge-2.py
+++ challenge-2.py
@@ -5,10 +5,13 @@ import requests
 import pandas as pd
 from pandas.io.json import json_normalize
 
-#obtener la última información de los commits
-iron_commits = requests.get('https://api.github.com/repos/ironhack-datalabs/madrid-oct-2018/commits/master')
-iron_commits.json()
+#obtener la última información de los commits en stats/commit_activity y transformarla a json
+iron_commits = requests.get('https://api.github.com/repos/ironhack-datalabs/madrid-oct-2018/stats/commit_activity')
+iron_commits = iron_commits.json()
 
-iron_commits.headers
-#Last modified = Thu, 07 Mar 2019 15:49:16 GMT
-#Desde el 7 marzo de 2019 no hay commits
+#Regresa una lista de los commits de las últimas semanas, el último realizado en marzo de este año
+iron_commits
+
+#A través de un f string imprimir el último valor, el cual representa la última semana: 0
+total = f"Number of commits last week: {iron_commits[-1]['total']}"
+print(total)
--- challenge-3.py
+++ challenge-3.py
@@ -5,6 +5,21 @@ import requests
 import pandas as pd
 from pandas.io.json import json_normalize
 
+#importar base64 para decodificar los la info de los requests
+import base64
 
-iron_scavenger = requests.get('https://api.github.com/repos/ironhack-datalabs/scavenger')
-iron_scavenger.json()
+#Realizar un doble requests del url, transformarlo a json y leer content_url en específico
+iron_scavenger = requests.get(requests.get('https://api.github.com/repos/ironhack-datalabs/scavenger', verify=False).json()['contents_url'][:-7], verify=False).json()
+
+#Crear dos listas vacías para agregar los nombres de los archivos y el mensaje de broma
+cold_joke = []
+names = []
+
+#Iterar sobre la variable de doble request y sobre un nuevo request que en específico tenga las url de los archivos secretos
+for inf in iron_scavenger:
+    for sec_files in requests.get(inf['url'], verify=False).json():
+        if type(sec_files) == str:
+            pass
+        elif 'scavengerhunt' in sec_files['name']:
+            names.append(sec_files['name'])
+            cold_joke.append(base64.b64decode(requests.get(sec_files['url'], verify=False).json()['content'])
